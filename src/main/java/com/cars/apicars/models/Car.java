e218a941e3cc5fd897bf61fa6d31e417dcc35c54
Nilson Junior <nilsonjrsilva97@gmail.com>
2022-09-13 19:42:56 -0300

    adding car repository

--- src/main/java/com/cars/apicars/models/Car.java
+++ src/main/java/com/cars/apicars/models/Car.java
@@ -1,10 +1,114 @@
 package com.cars.apicars.models;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name="tb_cars")
-public class Car {
+public class Car implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
 
+    private String vehicle;
+
+    private String brand;
+
+    private Integer year;
+
+    private String description;
+
+    private Boolean sold;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created", nullable = false)
+    private Date created;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated", nullable = false)
+    private Date updated;
+
+    @PrePersist
+    protected void onCreate() {
+        updated = created = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated = new Date();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getSold() {
+        return sold;
+    }
+
+    public void setSold(Boolean sold) {
+        this.sold = sold;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
 }
---
+++ src/main/java/com/cars/apicars/repositories/CarRepository.java
@@ -0,0 +1,20 @@
+package com.cars.apicars.repositories;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.cars.apirest.models.Car;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+    Car findById(long id);
+
+    @Query("SELECT COUNT(*) FROM Car WHERE sold = false")
+    int countCarNotSold();
+
+    int countAllByBrand(String brand);
+
+    @Query("SELECT COUNT(*) FROM Car WHERE year >= year AND year <= (year + 10)")
+    int carByYear(@Param("year") int year);
+}
